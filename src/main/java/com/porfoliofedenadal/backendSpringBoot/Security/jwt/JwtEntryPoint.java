ffda835ac0909f071ffffb59d7fa68d6fcd79028
fedenadal <fedenadal@hotmail.com>
2023-03-21 14:52:39 -0300

    correccion logger.error

--- src/main/java/com/porfoliofedenadal/backendSpringBoot/Security/jwt/JwtEntryPoint.java
+++ src/main/java/com/porfoliofedenadal/backendSpringBoot/Security/jwt/JwtEntryPoint.java
@@ -19,7 +19,7 @@ public class JwtEntryPoint implements AuthenticationEntryPoint {
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
         logger.error("Fallo el metodo commence");
-        getMessage(authException);
+        logger.error(authException.getMessage());
         response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
     }
 }
